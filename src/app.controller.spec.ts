52b150b8c8770032ebfe899badafb19fe346c6ee
Anika <munikamustafiz89@gmail.com>
2021-06-27 11:28:17 -0400

    add unit tests for getting all counters

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,9 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import faker = require('faker');
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const getDummyCounter = (): Counter => {
+  return { [faker.random.word()]: faker.datatype.number() };
+};
+
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -11,12 +18,21 @@ describe('AppController', () => {
       providers: [AppService],
     }).compile();
 
+    appService = app.get<AppService>(AppService);
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe.only('getAllCounters', () => {
+    it('should return an empty array if no counters are set', () => {
+      expect(appController.getAllCounters()).toEqual([]);
+    });
+
+    it('should return an array of counters', () => {
+      const counters = [getDummyCounter(), getDummyCounter()];
+
+      appService.counters = counters;
+
+      expect(appController.getAllCounters()).toBe(counters);
     });
   });
 });
